ade505af5f7f4ac47aebdc52b4432b2dff62c56c
unknown <narmada.vaddireddy97@gmail.com>
2023-03-03 21:29:12 +0530

    Added: Code to create the contacts in Address Book

---
+++ AddressBook.java
@@ -0,0 +1,5 @@
+public class AddressBook {
+Contacts details=new Contacts("Narmada", "Vaddireddy", "Akkampally", "Nalagonda",
+        "Telangana", "508243", "9603565087", "narmada.v@gmail.com");
+
+}
---
+++ Contacts.java
@@ -0,0 +1,93 @@
+public class Contacts {
+    private String FIRST_NAME;
+    private String LAST_NAME;
+    private String ADDRESS;
+    private String CITY;
+    private String STATE;
+    private String zip;
+    private String PHONE_NUMBER;
+    private String EMAIL_ID;
+
+    public Contacts(String FIRST_NAME, String LAST_NAME, String ADDRESS,
+                    String CITY, String STATE, String zip, String PHONE_NUMBER, String EMAIL_ID) {
+        this.FIRST_NAME = FIRST_NAME;
+        this.LAST_NAME = LAST_NAME;
+        this.ADDRESS = ADDRESS;
+        this.CITY = CITY;
+        this.STATE = STATE;
+        this.zip = zip;
+        this.PHONE_NUMBER = PHONE_NUMBER;
+        this.EMAIL_ID = EMAIL_ID;
+    }
+
+    public String getFIRST_NAME() {
+        return FIRST_NAME;
+    }
+
+    public void setFIRST_NAME(String FIRST_NAME) {
+        this.FIRST_NAME = FIRST_NAME;
+    }
+
+    public String getLAST_NAME() {
+        return LAST_NAME;
+    }
+
+    public void setLAST_NAME(String LAST_NAME) {
+        this.LAST_NAME = LAST_NAME;
+    }
+
+    public String getADDRESS() {
+        return ADDRESS;
+    }
+
+    public void setADDRESS(String ADDRESS) {
+        this.ADDRESS = ADDRESS;
+    }
+
+    public String getCITY() {
+        return CITY;
+    }
+
+    public void setCITY(String CITY) {
+        this.CITY = CITY;
+    }
+
+    public String getSTATE() {
+        return STATE;
+    }
+
+    public void setSTATE(String STATE) {
+        this.STATE = STATE;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPHONE_NUMBER() {
+        return PHONE_NUMBER;
+    }
+
+    public void setPHONE_NUMBER(String PHONE_NUMBER) {
+        this.PHONE_NUMBER = PHONE_NUMBER;
+    }
+
+    public String getEMAIL_ID() {
+        return EMAIL_ID;
+    }
+
+    public void setEMAIL_ID(String EMAIL_ID) {
+        this.EMAIL_ID = EMAIL_ID;
+    }
+
+    @Override
+    public String toString() {
+        return "First Name:" + FIRST_NAME + "\nLast Name:" + LAST_NAME + "\nAddress:" + ADDRESS +
+                "\nState:" + STATE + "\nCity:" + CITY + "\nZIP:" + zip + "\nPhone:" + PHONE_NUMBER + "\nEmail:" + EMAIL_ID;
+
+    }
+}
